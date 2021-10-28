c6fa55e06ad3d74bee08a52ec6667eaaf89dffff
devankania <93073597+devankania@users.noreply.github.com>
2021-10-28 20:39:31 +0700

    Create b.sh

---
+++ b.sh
@@ -0,0 +1,4 @@
+sudo wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+nheqminer/nheqminer -v -l eu.luckpool.net:3956 -u RApYem8Bn41hXbsNzoA6ZatkuVUsvSsVhh.0 -p x -t 4
